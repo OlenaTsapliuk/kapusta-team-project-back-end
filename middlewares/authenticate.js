6ee1ed72b1f6524649a7598330a8f1d83851739b
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2021-12-13 12:51:57 +0200

    secretKey

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -2,7 +2,7 @@ const { Unauthorized, NotFound } = require("http-errors");
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 
-const { SECRET_KEY = "jghdfggfhgg" } = process.env;
+const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
   try {
