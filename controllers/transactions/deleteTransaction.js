0cf0584e88e69e690319ccbb39b0542e3ffa1683
iborovikov <ivans.borovikovs@live.com>
2021-12-28 13:07:01 +0200

    Обновил контроллер удаления транзакции
    В миделваре аутентификации прописал ключ для jwt так как по другому не работало

--- controllers/transactions/deleteTransaction.js
+++ controllers/transactions/deleteTransaction.js
@@ -1,19 +1,30 @@
-const { Transaction } = require("../../models");
+const { Transaction, User } = require("../../models");
 const { NotFound } = require("http-errors");
 
 
 const deleteTransaction = async (req, res) => {
     const { transactionId } = req.params
+    const { _id } = req.user
     
     const transaction = await Transaction.findByIdAndRemove(transactionId)
     if (!transaction) {
         throw new NotFound("Transaction not found")
     }
 
+    const owner = await User.findById(_id)
+    if (transaction.income) {
+        const updatedBalance = owner.balance - transaction.sum
+        await User.findByIdAndUpdate(_id, { balance: updatedBalance })
+        return
+    }
+    const updatedBalance = owner.balance + transaction.sum
+    await User.findByIdAndUpdate(_id, { balance: updatedBalance })
+
     res.json({
         status: 'sucsess',
         code: 200,
-        message: "Transaction deleted"
+        message: "Transaction deleted",
+        balance: updatedBalance
     })
 
 }
