36b8ac83079f8c2f3409c61146577339ec889c8f
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2022-01-20 19:48:48 +0200

    update

--- controllers/auth/googleRedirect.js
+++ controllers/auth/googleRedirect.js
@@ -64,7 +64,7 @@ const googleRedirect = async (req, res) => {
   await User.findByIdAndUpdate(_id, { token });
 
   return res.redirect(
-    `${process.env.FRONT_URL}/api/users/google-redirect/?access_token=${token}&email=${user.email}`
+    `${process.env.FRONT_URL}/api/users/google-redirect/?access_token=${token}&email=${user.email}&balance=${user.balance}`
   );
 };
 
