98d3b8b9e87b48d1b8d72993595df5fb9ee48844
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2022-01-20 17:18:02 +0200

    edits

--- controllers/auth/googleRedirect.js
+++ controllers/auth/googleRedirect.js
@@ -63,9 +63,24 @@ const googleRedirect = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "7d" });
   await User.findByIdAndUpdate(_id, { token });
 
-  return res.redirect(
-    `${process.env.FRONT_URL}/api/users/google-redirect/?access_token=${token}&email=${user.email}`
-  );
+  return res
+    .redirect(
+      `${process.env.FRONT_URL}/api/users/google-redirect/?access_token=${token}&email=${user.email}`
+    )
+    .status(201)
+    .json({
+      status: "success",
+      code: 201,
+      data: {
+        token,
+        user: {
+          email,
+          id: user._id,
+          balance: user.balance,
+          balanceHasBeenSet: user.balanceHasBeenSet,
+        },
+      },
+    });
 };
 
 module.exports = googleRedirect;
